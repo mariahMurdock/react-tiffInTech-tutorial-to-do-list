d0a7943b04587b5580c40be2675519dbb9772454
mariahMurdock <mariahmurdock1227@gmail.com>
2020-12-18 12:08:26 -0800

    add a component?

--- src/App.js
+++ src/App.js
@@ -7,12 +7,23 @@ class App extends component {
       super(props);
 
       this.state={
-        
+        newItem="";
+        list: [];
       }
     }
     return(
       <div classname="App">
+        <div>
+           Add an Item...
+           <br/>
+           <input
+            type="text"
+            placeholder="Type item here..."
+            value={this.state.newItem}
+            onChange={e => this.updateInput("newItem", e.target.value)}
+           />
 
+        </div>
       </div>
     );
   },
