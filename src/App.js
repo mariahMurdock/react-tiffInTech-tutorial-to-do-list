a7160c3d6940cd2ddfdc3a548105a9ab4221e9fb
mariahMurdock <mariahmurdock1227@gmail.com>
2020-12-18 12:01:01 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class App extends component {
+  
+  render() {
+    constructor(props){
+      super(props);
+
+      this.state={
+        
+      }
+    }
+    return(
+      <div classname="App">
+
+      </div>
+    );
+  },
+}
+
+export default App;
