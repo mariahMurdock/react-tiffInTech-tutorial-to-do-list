953a0f6b87d4630b502786d7caf4dd4c284e456d
mariahMurdock <mariahmurdock1227@gmail.com>
2020-12-18 17:16:02 -0800

    add items to list feature

--- src/App.js
+++ src/App.js
@@ -1,32 +1,56 @@
-import React, { Component } from 'react';
+import React, { component } from 'react';
 
 class App extends component {
-  
-  render() {
-    constructor(props){
+    constructor(props) {
       super(props);
 
       this.state={
-        newItem="";
-        list: [];
+        newItem:"",
+        list: []
       }
     }
-    return(
-      <div classname="App">
-        <div>
-           Add an Item...
-           <br/>
-           <input
-            type="text"
-            placeholder="Type item here..."
-            value={this.state.newItem}
-            onChange={e => this.updateInput("newItem", e.target.value)}
-           />
 
+    addItem() {
+      // create item with unique id
+      const newItem={
+        id: 1 + Math.random(),
+        value: this.state.newItem.slice()
+      };
+
+      // copy current list of items
+      const list=[...this.state.list];
+
+      // add new item to list
+      list.push(newItem);
+      
+      // update state with new list and reset newItem input 
+      this.setState({
+        list,
+        newItem:""
+      });
+    }
+    render() {
+      return(
+        <div classname="App">
+          <div>
+             Add an Item...
+             <br/>
+             <input
+              type="text"
+              placeholder="Type item here..."
+              value={this.state.newItem}
+              onChange={e => this.updateInput("newItem", e.target.value)}
+             />
+              <button
+              onclick={() => this.addItem()} 
+              >
+                Add
+              </button>
+          </div>
         </div>
-      </div>
+    
     );
-  },
+  }
 }
 
 export default App;
